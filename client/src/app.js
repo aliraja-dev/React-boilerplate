189d105879fd525e62e69bd97f461a24c9f198e1
aliraja-dev <aliraja.en@gmail.com>
2022-02-21 16:03:20 -0500

    React-app-client setup

---
+++ client/src/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// We use Route in order to define the different routes of our application
+
+
+// We import all the components we need in our app
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Main page</h1>
+
+        </div>
+    );
+};
+
+export default App;
